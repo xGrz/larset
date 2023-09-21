c500a7baadbe33fd0f3b2f2bc881104260c1c2de
xGrz <33755434+xGrz@users.noreply.github.com>
2023-09-21 10:21:30 +0200

    Init project service provider

---
+++ src/LarSetServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+
+use Illuminate\Support\ServiceProvider;
+
+class LarSetServiceProvider extends ServiceProvider
+{
+   public function register(): void
+    {
+    }
+
+    public function boot(): void
+    {
+//        $this->publishes([__DIR__ . '/../config/config.php' => config_path('laragus.php')]);
+//
+//        $this->loadRoutesFrom(__DIR__ . '/../routes/api.php');
+    }
+
+}
